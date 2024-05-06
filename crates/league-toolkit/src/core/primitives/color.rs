a6fc8c28cf5e286098637b0d81962fa8cf838ee3
Alan Panayotov <alanpanayotov@gmail.com>
2024-05-06 19:29:00 +0100

    feat: impl Color::to_writer

--- crates/league-toolkit/src/core/primitives/color.rs
+++ crates/league-toolkit/src/core/primitives/color.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use byteorder::{ByteOrder, WriteBytesExt};
+
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
     pub r: f32,
@@ -11,6 +14,14 @@ impl Color {
 
     pub const fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
         Self { r, g, b, a }
+    }
+
+    pub fn to_writer<E: ByteOrder>(&self, writer: &mut (impl Write + ?Sized)) -> std::io::Result<()> {
+        writer.write_f32::<E>(self.r)?;
+        writer.write_f32::<E>(self.g)?;
+        writer.write_f32::<E>(self.b)?;
+        writer.write_f32::<E>(self.a)?;
+        Ok(())
     }
 }
 
