1622b36b8f50db8c61f74b9cb1aaf31ef20a54ba
Alan Panayotov <alanpanayotov@gmail.com>
2024-05-06 19:29:00 +0100

    chore: extract mesh read impl to own submodule

--- crates/league-toolkit/src/core/mesh/skinned/mod.rs
+++ crates/league-toolkit/src/core/mesh/skinned/mod.rs
@@ -1,20 +1,20 @@
 use std::io::Read;
 
+use byteorder::ReadBytesExt;
+use num_enum::TryFromPrimitive;
+
+pub use range::*;
+
 use crate::core::{
-    mem::{ElementName, IndexBuffer, IndexFormat, VertexBuffer, VertexBufferDescription},
+    mem::{ElementName, IndexBuffer, VertexBuffer},
     primitives::{AABB, Sphere},
 };
-
-use byteorder::{LittleEndian, ReadBytesExt};
-use num_enum::{TryFromPrimitive, TryFromPrimitiveError};
 
 use super::Result;
 
 mod range;
-pub use range::*;
-use crate::core::mesh::error::ParseError;
-
 mod vertex;
+mod read;
 
 const MAGIC: u32 = 0x00112233;
 
@@ -47,75 +47,6 @@ impl SkinnedMesh {
             vertex_buffer,
             index_buffer,
         }
-    }
-
-    pub fn from_reader<R: Read>(reader: &mut R) -> Result<Self> {
-        use crate::util::ReaderExt as _;
-        let magic = reader.read_u32::<LittleEndian>()?;
-        if magic != MAGIC {
-            return Err(ParseError::InvalidFileSignature);
-        }
-
-        let major = reader.read_u16::<LittleEndian>()?;
-        let minor = reader.read_u16::<LittleEndian>()?;
-        if major != 0 && major != 2 && major != 4 && minor != 1 {
-            return Err(ParseError::InvalidFileVersion(major, minor));
-        }
-
-        let index_count;
-        let vertex_count;
-        let mut ranges;
-        let mut vertex_declaration: VertexBufferDescription = vertex::BASIC.clone();
-        let mut _b_box = AABB::default();
-        let mut _b_sphere = Sphere::INFINITE;
-
-        if major == 0 {
-            index_count = reader.read_i32::<LittleEndian>()?;
-            vertex_count = reader.read_i32::<LittleEndian>()?;
-            ranges = vec![SkinnedMeshRange::new("Base", 0, 0, 0, 0)]
-        } else {
-            let range_len = reader.read_u32::<LittleEndian>()? as usize;
-            ranges = Vec::with_capacity(range_len);
-            for _ in 0..range_len {
-                ranges.push(SkinnedMeshRange::from_reader(reader)?)
-            }
-
-            if major == 4 {
-                let _flags = reader.read_u32::<LittleEndian>()?;
-            }
-
-            index_count = reader.read_i32::<LittleEndian>()?;
-            vertex_count = reader.read_i32::<LittleEndian>()?;
-
-            if major == 4 {
-                let vertex_size = reader.read_u32::<LittleEndian>()?;
-                let vertex_type: SkinnedMeshVertexType = reader
-                    .read_u32::<LittleEndian>()?
-                    .try_into()
-                    .map_err(|e: TryFromPrimitiveError<SkinnedMeshVertexType>| ParseError::InvalidField("vertex type", e.number.to_string()))?;
-                vertex_declaration = match (vertex_size, vertex_type) {
-                    (52, SkinnedMeshVertexType::Basic) => vertex::BASIC.clone(),
-                    (56, SkinnedMeshVertexType::Color) => vertex::COLOR.clone(),
-                    (72, SkinnedMeshVertexType::Tangent) => vertex::TANGENT.clone(),
-                    _ => {
-                        return Err(ParseError::InvalidField("vertex type/size", format!("{vertex_type:?}: {vertex_size}")));
-                    }
-                };
-
-                _b_box = reader.read_bbox_f32::<LittleEndian>()?;
-                _b_sphere = reader.read_sphere_f32::<LittleEndian>()?;
-            }
-        }
-
-        let mut index_buffer = vec![0; (index_count as usize) * IndexFormat::U16.size()];
-        reader.read_exact(&mut index_buffer)?;
-        let index_buffer = IndexBuffer::new(crate::core::mem::IndexFormat::U16, index_buffer);
-
-        let mut vertex_buffer = vec![0; vertex_declaration.vertex_size() * vertex_count as usize];
-        reader.read_exact(&mut vertex_buffer)?;
-        let vertex_buffer = vertex_declaration.into_vertex_buffer(vertex_buffer);
-
-        Ok(Self::new(ranges, vertex_buffer, index_buffer))
     }
 
     pub fn aabb(&self) -> AABB<f32> {
---
+++ crates/league-toolkit/src/core/mesh/static/read.rs
@@ -0,0 +1,75 @@
+use std::io::Read;
+use byteorder::{LittleEndian, ReadBytesExt};
+use log::debug;
+use vecmath::Vector3;
+use crate::core::mesh::error::ParseError;
+use crate::core::mesh::r#static::MAGIC;
+use crate::core::mesh::{StaticMesh, StaticMeshFace};
+use crate::core::primitives::Color;
+
+impl StaticMesh {
+    pub fn from_reader<R: Read>(reader: &mut R) -> crate::core::mesh::Result<Self> {
+        use crate::util::ReaderExt as _;
+        let mut buf: [u8; 8] = [0; 8];
+        reader.read_exact(&mut buf)?;
+        if MAGIC != buf {
+            return Err(ParseError::InvalidFileSignature);
+        }
+
+        let major = reader.read_u16::<LittleEndian>()?;
+        let minor = reader.read_u16::<LittleEndian>()?;
+        debug!("version: {major}.{minor}");
+
+        // there are versions [2][1] and [1][1] as well
+        if major != 2 && major != 3 && minor != 1 {
+            return Err(ParseError::InvalidFileVersion(major, minor));
+        }
+
+        let name = reader.read_padded_string::<LittleEndian, 128>()?;
+        debug!("name: {name}");
+
+        let vertex_count = reader.read_i32::<LittleEndian>()?;
+        let face_count = reader.read_i32::<LittleEndian>()?;
+
+        let _flags = reader.read_u32::<LittleEndian>()?; // TODO (alan): handle StaticMeshFlags
+        let _bounding_box = reader.read_bbox_f32::<LittleEndian>()?;
+
+        let has_vertex_colors = match (major, minor) {
+            (3.., 2..) => reader.read_i32::<LittleEndian>()? == 1,
+            _ => false,
+        };
+
+        // TODO (alan): try some byte reinterp here
+        let mut vertices: Vec<Vector3<f32>> = Vec::with_capacity(vertex_count as usize);
+        for _ in 0..vertex_count {
+            vertices.push(reader.read_vector3_f32::<LittleEndian>()?);
+        }
+
+        let vertex_colors: Option<Vec<Color>> = match has_vertex_colors {
+            true => {
+                let mut v = Vec::with_capacity(vertex_count as usize);
+                for _ in 0..vertex_count {
+                    v.push(reader.read_color::<LittleEndian>()?);
+                }
+                Some(v)
+            }
+            false => None,
+        };
+
+        let _central_point = reader.read_vector3_f32::<LittleEndian>()?;
+
+        let mut faces = Vec::with_capacity(face_count as usize);
+        for _ in 0..face_count {
+            faces.push(StaticMeshFace::from_reader(reader)?);
+        }
+
+        // TODO (alan): read face vertex colors or something (StaticMeshFlags::HasVcp)
+
+        Ok(Self {
+            name,
+            vertices,
+            faces,
+            vertex_colors,
+        })
+    }
+}
