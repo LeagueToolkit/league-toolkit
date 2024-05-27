e720c3186aebc188b5410b55319231007e4cf4b6
Alan Panayotov <alanpanayotov@gmail.com>
2024-05-27 01:09:17 +0100

    feat: InvalidField error + move error to own module

---
+++ crates/league-toolkit/src/core/mesh/error.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, thiserror::Error)]
+pub enum ParseError {
+    #[error("Invalid file signature")]
+    InvalidFileSignature,
+    #[error("Invalid file version '{0}.{1}'")]
+    InvalidFileVersion(u16, u16),
+    #[error("Invalid '{0}' - got '{1}'")]
+    InvalidField(&'static str, String),
+    #[error("IO Error - {0}")]
+    ReaderError(#[from] std::io::Error),
+    #[error("UTF-8 Error - {0}")]
+    Utf8Error(#[from] std::str::Utf8Error),
+}
