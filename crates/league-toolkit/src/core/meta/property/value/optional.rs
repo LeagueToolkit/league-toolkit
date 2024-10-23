fc567ec6b33a7ce61e4240e33c5d918c009b4126
Alan Panayotov <alanpanayotov@gmail.com>
2024-10-23 19:02:15 +0100

    feat: impl WriteProperty for OptionalValue

--- crates/league-toolkit/src/core/meta/property/value/optional.rs
+++ crates/league-toolkit/src/core/meta/property/value/optional.rs
@@ -1,23 +1,29 @@
 use crate::core::meta::{
-    traits::{PropertyValue, ReadProperty},
+    property::BinPropertyKind,
+    traits::{PropertyValue, ReadProperty, WriteProperty},
     ParseError,
 };
 
+// I am not a fan of the double tagging, but fixing it would be a whole ordeal (and might not be
+// possible with static dispatch?).
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, PartialEq, Debug)]
-pub struct OptionalValue(pub Option<Box<PropertyValueEnum>>);
+pub struct OptionalValue(pub BinPropertyKind, pub Option<Box<PropertyValueEnum>>);
 
 impl PropertyValue for OptionalValue {
     fn size_no_header(&self) -> usize {
-        2 + match &self.0 {
+        2 + match &self.1 {
             Some(inner) => inner.size_no_header(),
             None => 0,
         }
     }
 }
 
-use super::{super::super::traits::ReaderExt as _, PropertyValueEnum};
-use crate::util::ReaderExt as _;
+use super::{
+    super::super::traits::{ReaderExt as _, WriterExt as _},
+    PropertyValueEnum,
+};
+use crate::util::{ReaderExt as _, WriterExt as _};
 impl ReadProperty for OptionalValue {
     fn from_reader<R: std::io::Read + std::io::Seek>(
         reader: &mut R,
@@ -30,9 +36,27 @@ impl ReadProperty for OptionalValue {
 
         let is_some = reader.read_bool()?;
 
-        Ok(Self(match is_some {
-            true => Some(kind.read(reader, legacy)?.into()),
-            false => None,
-        }))
+        Ok(Self(
+            kind,
+            match is_some {
+                true => Some(kind.read(reader, legacy)?.into()),
+                false => None,
+            },
+        ))
+    }
+}
+impl WriteProperty for OptionalValue {
+    fn to_writer<R: std::io::Write + ?Sized>(
+        &self,
+        writer: &mut R,
+        _legacy: bool,
+    ) -> Result<(), std::io::Error> {
+        writer.write_property_kind(self.0)?;
+        writer.write_bool(self.1.is_some())?;
+        if let Some(value) = &self.1 {
+            value.to_writer(writer)?;
+        }
+
+        Ok(())
     }
 }
