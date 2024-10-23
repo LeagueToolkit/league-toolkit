c61abffd1b4c0f05697eeb6a270e9e20178cc55b
Alan Panayotov <alanpanayotov@gmail.com>
2024-10-23 19:01:56 +0100

    feat: impl WriteProperty for primitives

--- crates/league-toolkit/src/core/meta/property/value/primitives.rs
+++ crates/league-toolkit/src/core/meta/property/value/primitives.rs
@@ -1,7 +1,7 @@
-use crate::core::meta::traits::{PropertyValue, ReadProperty};
+use crate::core::meta::traits::{PropertyValue, ReadProperty, WriteProperty};
 
 macro_rules! impl_prim {
-    ($name:tt, $rust:tt, [$($derive:tt),*], $($method:tt)*) => {
+    ($name:tt, $rust:tt, [$($derive:tt),*], $method:ident $(::<$endian:ident>)?, $($write_value:tt)*) => {
         #[repr(transparent)]
         #[derive(Clone, Debug, PartialEq, $($derive),*)]
         #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -18,49 +18,58 @@ macro_rules! impl_prim {
                 reader: &mut R,
                 _legacy: bool,
             ) -> Result<Self, crate::core::meta::ParseError> {
-                Ok(Self(reader.$($method)*()?))
+                Ok(Self(paste::paste!(reader.[<read_ $method>]::<$($endian,)*>()?)))
+            }
+        }
+        impl WriteProperty for $name {
+            fn to_writer<W: std::io::Write + ?Sized>(
+                &self,
+                writer: &mut W,
+                _legacy: bool,
+            ) -> Result<(), std::io::Error> {
+                paste::paste!(writer.[<write_ $method>]::<$($endian,)*>(self.$($write_value)*))
             }
         }
     };
 }
 
 use crate::core::primitives::Color;
-use crate::util::ReaderExt;
-use byteorder::{ReadBytesExt, LE};
+use crate::util::{ReaderExt, WriterExt};
+use byteorder::{ReadBytesExt, WriteBytesExt, LE};
 use glam::{Mat4, Vec2, Vec3, Vec4};
 
 // A "primitive" in this case is just a PropertyValue that just encapsulates
 // a single struct/rust primitive.
 
-impl_prim!(BoolValue, bool, [Eq, Hash], read_bool);
+impl_prim!(BoolValue, bool, [Eq, Hash], bool, 0);
 
 // https://github.com/LeagueToolkit/league-toolkit/pull/6#discussion_r1809366173
 // > Afaik this is leftover from before bitfield support was added to league.
 // > This type is also not a primitive, meaning it can't be used as a key for map.
 // - moonshadow
-impl_prim!(BitBoolValue, bool, [Eq, Hash], read_bool);
+impl_prim!(BitBoolValue, bool, [Eq, Hash], bool, 0);
 
-impl_prim!(I8Value, i8, [Eq, Hash], read_i8);
-impl_prim!(U8Value, u8, [Eq, Hash], read_u8);
+impl_prim!(I8Value, i8, [Eq, Hash], i8, 0);
+impl_prim!(U8Value, u8, [Eq, Hash], u8, 0);
 
-impl_prim!(I16Value, i16, [Eq, Hash], read_i16::<LE>);
-impl_prim!(U16Value, u16, [Eq, Hash], read_u16::<LE>);
+impl_prim!(I16Value, i16, [Eq, Hash], i16::<LE>, 0);
+impl_prim!(U16Value, u16, [Eq, Hash], u16::<LE>, 0);
 
-impl_prim!(I32Value, i32, [Eq, Hash], read_i32::<LE>);
-impl_prim!(U32Value, u32, [Eq, Hash], read_u32::<LE>);
+impl_prim!(I32Value, i32, [Eq, Hash], i32::<LE>, 0);
+impl_prim!(U32Value, u32, [Eq, Hash], u32::<LE>, 0);
 
-impl_prim!(I64Value, i64, [Eq, Hash], read_i64::<LE>);
-impl_prim!(U64Value, u64, [Eq, Hash], read_u64::<LE>);
+impl_prim!(I64Value, i64, [Eq, Hash], i64::<LE>, 0);
+impl_prim!(U64Value, u64, [Eq, Hash], u64::<LE>, 0);
 
-impl_prim!(F32Value, f32, [], read_f32::<LE>);
+impl_prim!(F32Value, f32, [], f32::<LE>, 0);
 
-impl_prim!(Vector2Value, Vec2, [], read_vec2::<LE>);
-impl_prim!(Vector3Value, Vec3, [], read_vec3::<LE>);
-impl_prim!(Vector4Value, Vec4, [], read_vec4::<LE>);
-impl_prim!(Matrix44Value, Mat4, [], read_mat4_row_major::<LE>);
+impl_prim!(Vector2Value, Vec2, [], vec2::<LE>, 0);
+impl_prim!(Vector3Value, Vec3, [], vec3::<LE>, 0);
+impl_prim!(Vector4Value, Vec4, [], vec4::<LE>, 0);
+impl_prim!(Matrix44Value, Mat4, [], mat4_row_major::<LE>, 0);
 
 type ColorU8 = Color<u8>;
-impl_prim!(ColorValue, ColorU8, [], read_color_u8);
-impl_prim!(HashValue, u32, [Eq, Hash], read_u32::<LE>);
-impl_prim!(WadChunkLinkValue, u64, [Eq, Hash], read_u64::<LE>);
-impl_prim!(ObjectLinkValue, u32, [Eq, Hash], read_u32::<LE>);
+impl_prim!(ColorValue, ColorU8, [], color_u8, 0.as_ref());
+impl_prim!(HashValue, u32, [Eq, Hash], u32::<LE>, 0);
+impl_prim!(WadChunkLinkValue, u64, [Eq, Hash], u64::<LE>, 0);
+impl_prim!(ObjectLinkValue, u32, [Eq, Hash], u32::<LE>, 0);
