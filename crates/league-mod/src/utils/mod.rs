da876883301647cb990c1c9ca9bdfa3d2a00bf76
Crauzer <filip.quitko@gmail.com>
2024-11-08 21:43:02 +0100

    chore: add test

--- crates/league-mod/src/utils/mod.rs
+++ crates/league-mod/src/utils/mod.rs
@@ -1,13 +1,32 @@
 use eyre::eyre;
 use regex::Regex;
 
+pub fn is_valid_mod_name(name: impl AsRef<str>) -> bool {
+    Regex::new(r"^[[:word:]-]+$")
+        .unwrap()
+        .is_match(name.as_ref())
+}
+
 pub fn validate_mod_name(name: impl AsRef<str>) -> eyre::Result<()> {
-    let check = Regex::new(r"^[[:word:]-]+$").unwrap();
-    if !check.is_match(name.as_ref()) {
+    if !is_valid_mod_name(name) {
         return Err(eyre!(
             "Invalid mod name, must be alphanumeric and contain no spaces or special characters"
         ));
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_mod_name_valid() {
+        assert!(is_valid_mod_name("test"));
+        assert!(is_valid_mod_name("test-123"));
+        assert!(!is_valid_mod_name("test 123"));
+        assert!(!is_valid_mod_name("test!123"));
+        assert!(!is_valid_mod_name("Nice mod: ([test])@"));
+    }
 }
