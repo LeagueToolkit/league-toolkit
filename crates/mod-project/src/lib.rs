b0da9798825c5fc1a4046b07943f1f3cae038e73
Crauzer <filip.quitko@gmail.com>
2024-11-08 20:31:51 +0100

    feat: add mod project format (#15)

---
+++ crates/mod-project/src/lib.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd)]
+pub struct ModProject {
+    pub name: String,
+    pub version: String,
+    pub description: String,
+    pub authors: Vec<ModProjectAuthor>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd)]
+#[serde(untagged)]
+pub enum ModProjectAuthor {
+    Name(String),
+    Role { name: String, role: String },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_project() {
+        let project: ModProject =
+            toml::from_str(include_str!("../test-data/modproject.toml")).unwrap();
+
+        assert_eq!(
+            project,
+            ModProject {
+                name: "test".to_string(),
+                version: "0.1.0".to_string(),
+                description: "test".to_string(),
+                authors: vec![
+                    ModProjectAuthor::Name("test".to_string()),
+                    ModProjectAuthor::Role {
+                        name: "test 2".to_string(),
+                        role: "developer".to_string(),
+                    },
+                ],
+            }
+        );
+    }
+}
