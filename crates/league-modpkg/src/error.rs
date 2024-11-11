f8b39ad57fbf682d0ca80e8a988e595a77ab2a3d
Crauzer <filip.quitko@gmail.com>
2024-11-11 21:51:16 +0100

    feat: rework modpkg format

--- crates/league-modpkg/src/error.rs
+++ crates/league-modpkg/src/error.rs
@@ -7,6 +7,12 @@ pub enum ModpkgError {
     #[error("IO error: {0}")]
     IoExtError(#[from] io_ext::ReaderError),
 
+    #[error("Invalid modpkg header size: {header_size}, actual size: {actual_size}")]
+    InvalidHeaderSize { header_size: u32, actual_size: u64 },
+    #[error("Chunks are not in ascending order: previous: {previous}, current: {current}")]
+    UnsortedChunks { previous: u64, current: u64 },
+    #[error("Missing base layer")]
+    MissingBaseLayer,
     #[error("Invalid modpkg compression type: {0}")]
     InvalidCompressionType(u8),
     #[error("Invalid modpkg license type: {0}")]
@@ -17,4 +23,9 @@ pub enum ModpkgError {
     InvalidVersion(u32),
     #[error("Duplicate chunk: {0}")]
     DuplicateChunk(u64),
+
+    #[error("Msgpack decode error: {0}")]
+    MsgpackDecode(#[from] rmp_serde::decode::Error),
+    #[error("Msgpack encode error: {0}")]
+    MsgpackEncode(#[from] rmp_serde::encode::Error),
 }
