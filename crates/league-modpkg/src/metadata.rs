a6a51cc6beb747bf577d0d376aec019ab3d935ac
Alan Panayotov <alanpanayotov@gmail.com>
2025-02-07 11:21:26 +0000

    feat: port ModpkgLicense/ModpkgAuthor to binrw

--- crates/league-modpkg/src/metadata.rs
+++ crates/league-modpkg/src/metadata.rs
@@ -1,85 +1,70 @@
-use byteorder::{ReadBytesExt as _, LE};
+use binrw::binrw;
 
-use crate::license::ModpkgLicense;
-use crate::utils::length_prefixed_string_size;
-use crate::{error::ModpkgError, utils::non_empty_string};
-use io_ext::{ReaderExt as _, WriterExt as _};
+use crate::{
+    license::ModpkgLicense,
+    utils::{optional_string_len, optional_string_read, optional_string_write},
+};
 use serde::{Deserialize, Serialize};
-use std::io::{BufReader, Read, Write};
 
+#[binrw]
+#[brw(little)]
 #[derive(Debug, PartialEq, Default)]
 pub struct ModpkgMetadata {
+    #[br(temp)]
+    #[bw(calc = name.len() as u32)]
+    name_len: u32,
+    #[br(count = name_len, try_map = String::from_utf8)]
+    #[bw(map = |s| s.as_bytes().to_vec())]
     name: String,
+
+    #[br(temp)]
+    #[bw(calc = display_name.len() as u32)]
+    display_name_len: u32,
+    #[br(count = display_name_len, try_map = String::from_utf8)]
+    #[bw(map = |s| s.as_bytes().to_vec())]
     display_name: String,
+
+    #[br(temp)]
+    #[bw(calc = optional_string_len(description) as u32)]
+    description_len: u32,
+    #[brw(if(description_len > 0))]
+    #[bw(map = optional_string_write)]
+    #[br(count = description_len, try_map = optional_string_read)]
     description: Option<String>,
+
+    #[br(temp)]
+    #[bw(calc = version.len() as u32)]
+    version_len: u32,
+    #[br(count = version_len, try_map = String::from_utf8)]
+    #[bw(map = |s| s.as_bytes().to_vec())]
     version: String,
+
+    #[br(temp)]
+    #[bw(calc = optional_string_len(distributor) as u32)]
+    distributor_len: u32,
+    #[brw(if(distributor_len> 0))]
+    #[bw(map = optional_string_write)]
+    #[br(count = distributor_len, try_map = optional_string_read)]
     distributor: Option<String>,
+
+    #[br(temp)]
+    #[bw(calc = (authors.len()) as u32)]
+    author_count: u32,
+    #[br(count = author_count)]
     authors: Vec<ModpkgAuthor>,
+
     license: ModpkgLicense,
 }
 
 impl ModpkgMetadata {
-    pub fn read(reader: &mut BufReader<impl Read>) -> Result<Self, ModpkgError> {
-        let name = reader.read_len_prefixed_string::<LE>()?;
-        let display_name = reader.read_len_prefixed_string::<LE>()?;
-        let description = non_empty_string(reader.read_len_prefixed_string::<LE>()?);
-        let version = reader.read_len_prefixed_string::<LE>()?;
-        let distributor = non_empty_string(reader.read_len_prefixed_string::<LE>()?);
-
-        let authors = Self::read_authors(reader)?;
-        let license = ModpkgLicense::read(reader)?;
-
-        Ok(Self {
-            name,
-            display_name,
-            description,
-            version,
-            distributor,
-            authors,
-            license,
-        })
-    }
-
-    pub fn write(&self, writer: &mut impl Write) -> Result<(), ModpkgError> {
-        writer.write_len_prefixed_string_better::<LE>(&self.name)?;
-        writer.write_len_prefixed_string_better::<LE>(&self.display_name)?;
-        writer
-            .write_len_prefixed_string_better::<LE>(self.description.as_ref().map_or("", |v| v))?;
-        writer.write_len_prefixed_string_better::<LE>(&self.version)?;
-        writer
-            .write_len_prefixed_string_better::<LE>(self.distributor.as_ref().map_or("", |v| v))?;
-
-        for author in &self.authors {
-            author.write(writer)?;
-        }
-
-        self.license.write(writer)?;
-
-        Ok(())
-    }
-
-    fn read_authors(reader: &mut BufReader<impl Read>) -> Result<Vec<ModpkgAuthor>, ModpkgError> {
-        let count = reader.read_u32::<LE>()?;
-        let mut authors = Vec::with_capacity(count as usize);
-        for _ in 0..count {
-            authors.push(ModpkgAuthor::read(reader)?);
-        }
-
-        Ok(authors)
-    }
-
     pub fn size(&self) -> usize {
-        let mut size = 0;
-
-        size += length_prefixed_string_size(&self.name);
-        size += length_prefixed_string_size(&self.display_name);
-        size += length_prefixed_string_size(self.description.as_ref().map_or("", |v| v));
-        size += length_prefixed_string_size(&self.version);
-        size += length_prefixed_string_size(self.distributor.as_ref().map_or("", |v| v));
-        size += 4 + self.authors.iter().map(|a| a.size()).sum::<usize>();
-        size += self.license.size();
-
-        size
+        (self.name.len() + size_of::<u32>())
+            + (self.display_name.len() + size_of::<u32>())
+            + (optional_string_len(&self.description) + size_of::<u32>())
+            + (self.version.len() + size_of::<u32>())
+            + (optional_string_len(&self.distributor) + size_of::<u32>())
+            + (self.authors.iter().map(|a| a.size()).sum::<usize>() + size_of::<u32>())
+            + self.license().size()
     }
 }
 
@@ -107,9 +92,24 @@ impl ModpkgMetadata {
     }
 }
 
+#[binrw]
+#[brw(little)]
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ModpkgAuthor {
+    #[br(temp)]
+    #[bw( calc = name.len() as u32 )]
+    name_len: u32,
+    #[br(count = name_len, try_map = String::from_utf8)]
+    #[bw( map = |s| s.as_bytes().to_vec() )]
     name: String,
+
+    #[br(temp)]
+    #[bw( calc = role.as_ref().map(|n| n.len() as u32).unwrap_or_default() )]
+    role_len: u32,
+
+    #[brw(if(role_len > 0))]
+    #[bw( map = |s| s.as_ref().map(|s| s.as_bytes().to_vec()) )]
+    #[br(count = role_len, try_map = |s| String::from_utf8(s).map(Some))]
     role: Option<String>,
 }
 
@@ -123,38 +123,36 @@ impl ModpkgAuthor {
 }
 
 impl ModpkgAuthor {
-    pub fn read(reader: &mut BufReader<impl Read>) -> Result<Self, ModpkgError> {
-        let name = reader.read_len_prefixed_string::<LE>()?;
-        let role = non_empty_string(reader.read_len_prefixed_string::<LE>()?);
-
-        Ok(Self { name, role })
-    }
-
-    pub fn write(&self, writer: &mut impl Write) -> Result<(), ModpkgError> {
-        writer.write_len_prefixed_string_better::<LE>(&self.name)?;
-        writer.write_len_prefixed_string_better::<LE>(self.role.as_ref().map_or("", |v| v))?;
-        Ok(())
-    }
-
     pub fn size(&self) -> usize {
-        length_prefixed_string_size(&self.name)
-            + length_prefixed_string_size(self.role.as_ref().map_or("", |v| v))
+        (self.name.len() + size_of::<u32>()) + (optional_string_len(&self.role) + size_of::<u32>())
     }
 }
 
+// TODO: use proptest here
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
+    use binrw::BinWrite;
+
     use super::*;
 
     #[test]
-    fn test_empty_size() {
+    fn test_empty_metadata_size() {
         let metadata = ModpkgMetadata::default();
 
-        assert_eq!(metadata.size(), 25);
+        let mut buf = Cursor::new(Vec::with_capacity(metadata.size() + 512));
+        metadata.write(&mut buf).unwrap();
+
+        assert_eq!(
+            metadata.size(),
+            buf.into_inner().len(),
+            "comparing reported size with real size"
+        );
     }
 
     #[test]
-    fn test_size() {
+    fn test_metadata_size() {
         let mod_name = "test".to_string();
         let display_name = "test".to_string();
         let description = "test".to_string();
@@ -163,19 +161,8 @@ mod tests {
         let author_name = "test".to_string();
         let author_role = "test".to_string();
         let license = ModpkgLicense::Spdx {
-            spdx_id: "test".to_string(),
+            spdx_id: "test".to_string().into(),
         };
-
-        let expected_size = length_prefixed_string_size(&mod_name)
-            + length_prefixed_string_size(&display_name)
-            + length_prefixed_string_size(&description)
-            + length_prefixed_string_size(&version)
-            + length_prefixed_string_size(&distributor)
-            + 4
-            + length_prefixed_string_size(&author_name)
-            + length_prefixed_string_size(&author_role)
-            + 1
-            + 8;
 
         let metadata = ModpkgMetadata {
             name: mod_name,
@@ -189,7 +176,13 @@ mod tests {
             }],
             license,
         };
+        let mut buf = Cursor::new(Vec::with_capacity(metadata.size() + 512));
+        metadata.write(&mut buf).unwrap();
 
-        assert_eq!(metadata.size(), expected_size);
+        assert_eq!(
+            metadata.size(),
+            buf.into_inner().len(),
+            "comparing reported size with real size"
+        );
     }
 }
