03251ae9049e20977e7af7509e7ab88c2449caff
Alan Panayotov <alanpanayotov@gmail.com>
2025-02-07 11:25:51 +0000

    feat: add binrw utils

--- crates/league-modpkg/src/utils.rs
+++ crates/league-modpkg/src/utils.rs
@@ -1,10 +1,10 @@
-pub fn non_empty_string(value: String) -> Option<String> {
-    match value.is_empty() {
-        true => None,
-        false => Some(value),
-    }
+pub fn optional_string_len<'a>(string: impl Into<Option<&'a String>>) -> usize {
+    string.into().as_ref().map(|n| n.len()).unwrap_or_default()
 }
 
-pub fn length_prefixed_string_size(value: impl AsRef<str>) -> usize {
-    value.as_ref().len() + 4
+pub fn optional_string_write(s: &Option<String>) -> Option<Vec<u8>> {
+    s.as_ref().map(|s| s.as_bytes().to_vec())
+}
+pub fn optional_string_read(s: Vec<u8>) -> Result<Option<String>, std::string::FromUtf8Error> {
+    String::from_utf8(s).map(Some)
 }
