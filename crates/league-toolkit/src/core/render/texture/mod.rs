0a90e1f964459bd404b086d7a2288a96d8591d14
Alan Panayotov <alanpanayotov@gmail.com>
2025-02-12 23:42:32 +0000

    feat: add width/height methods to CompressedTexture

--- crates/league-toolkit/src/core/render/texture/mod.rs
+++ crates/league-toolkit/src/core/render/texture/mod.rs
@@ -8,6 +8,25 @@ pub enum CompressedTexture {
     Tex(image_dds::Surface<Vec<u8>>),
 }
 
+impl CompressedTexture {
+    #[inline]
+    #[must_use]
+    pub fn width(&self) -> u32 {
+        match self {
+            CompressedTexture::Dds(dds) => dds.get_width(),
+            CompressedTexture::Tex(tex) => tex.width,
+        }
+    }
+    #[inline]
+    #[must_use]
+    pub fn height(&self) -> u32 {
+        match self {
+            CompressedTexture::Dds(dds) => dds.get_height(),
+            CompressedTexture::Tex(tex) => tex.height,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum UncompressedTexture {
     Dds(ddsfile::Dds),
