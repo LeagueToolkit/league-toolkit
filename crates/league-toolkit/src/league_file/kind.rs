67e46de7ca4005a50ed642c317254baee526840c
Alan Panayotov <alanpanayotov@gmail.com>
2025-02-24 00:14:03 +0000

    feat: identify_league_file -> LeagueFileKind::identify_from_bytes

--- crates/league-toolkit/src/league_file/kind.rs
+++ crates/league-toolkit/src/league_file/kind.rs
@@ -1,3 +1,5 @@
+use super::pattern::LEAGUE_FILE_MAGIC_BYTES;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "snake_case"))]
@@ -29,9 +31,16 @@ pub enum LeagueFileKind {
 impl LeagueFileKind {
     #[inline]
     #[must_use]
-    /// The extension for this file type (.anm, .mapgeo, .bin, etc)
-    pub fn extension(&self) -> &'static str {
-        match self {
+    /// The extension for this file type (anm, mapgeo, bin, etc)
+    /// ```
+    /// # use league_toolkit::league_file::LeagueFileKind;
+    /// assert_eq!(LeagueFileKind::Animation.extension(), Some("anm"));
+    /// assert_eq!(LeagueFileKind::StaticMeshAscii.extension(), Some("sco"));
+    /// assert_eq!(LeagueFileKind::Unknown.extension(), None);
+    ///
+    pub fn extension(&self) -> Option<&'static str> {
+        Some(match self {
+            Self::Unknown => return None,
             Self::Animation => "anm",
             Self::Jpeg => "jpg",
             Self::LightGrid => "lightgrid",
@@ -48,16 +57,21 @@ impl LeagueFileKind {
             Self::StaticMeshBinary => "scb",
             Self::Texture => "tex",
             Self::TextureDds => "dds",
-            Self::Unknown => "",
             Self::WorldGeometry => "wgeo",
             Self::WwiseBank => "bnk",
             Self::WwisePackage => "wpk",
             Self::Svg => "svg",
-        }
+        })
     }
 
     #[must_use]
-    /// Infer the file type from the extension. Works with or without a preceding '.'.
+    /// Infer the file type from the extension. Works with or without a preceding `'.'`.
+    /// ```
+    /// # use league_toolkit::league_file::LeagueFileKind;
+    /// #
+    /// assert_eq!(LeagueFileKind::from_extension("png"), LeagueFileKind::Png);
+    /// assert_eq!(LeagueFileKind::from_extension(".png"), LeagueFileKind::Png);
+    /// ```
     pub fn from_extension(extension: impl AsRef<str>) -> LeagueFileKind {
         let extension = extension.as_ref();
         if extension.is_empty() {
@@ -90,5 +104,42 @@ impl LeagueFileKind {
             "wpk" => Self::WwisePackage,
             _ => Self::Unknown,
         }
+    }
+
+    /// Identify the type of league file from the magic at the start of the file. You must provide at
+    /// least [`MAX_MAGIC_SIZE`] bytes of data to be able to detect all file types.
+    ///
+    /// # Examples
+    /// ```
+    /// # use league_toolkit::league_file::*;
+    /// #
+    /// let data = b"r3d2skltblahblahblahblah";
+    /// let kind = LeagueFileKind::identify_from_bytes(data);
+    /// assert_eq!(kind, LeagueFileKind::Skeleton);
+    /// ```
+    ///
+    ///
+    /// ## Identifying from a reader
+    /// ```
+    /// # use std::fs::File;
+    /// # use std::io::{self, Cursor, Read};
+    /// # use league_toolkit::league_file::*;
+    /// #
+    /// let mut reader = Cursor::new([0x33, 0x22, 0x11, 0x00, 0xDE, 0xAD, 0xBE, 0xEF]);
+    /// let mut buffer = [0; MAX_MAGIC_SIZE];
+    /// reader.read(&mut buffer)?;
+    ///
+    /// let kind = LeagueFileKind::identify_from_bytes(&buffer);
+    /// assert_eq!(kind, LeagueFileKind::SimpleSkin);
+    /// # Ok::<(), io::Error>(())
+    /// ```
+    pub fn identify_from_bytes(data: &[u8]) -> LeagueFileKind {
+        for magic_byte in LEAGUE_FILE_MAGIC_BYTES.iter() {
+            if magic_byte.matches(data) {
+                return magic_byte.kind;
+            }
+        }
+
+        LeagueFileKind::Unknown
     }
 }
--- crates/league-toolkit/src/league_file/mod.rs
+++ crates/league-toolkit/src/league_file/mod.rs
@@ -1,43 +1,8 @@
+//! Utility module for identifying/working with League of Legends file types.
+//!
+//! See [`LeagueFileKind`] for more information.
 mod kind;
 mod pattern;
 
 pub use kind::*;
-use pattern::LEAGUE_FILE_MAGIC_BYTES;
 pub use pattern::MAX_MAGIC_SIZE;
-
-/// Identify the type of league file from the magic at the start of the file. You must provide at
-/// least [`MAX_MAGIC_SIZE`] bytes of data to be able to detect all file types.
-///
-/// # Examples
-/// ```
-/// # use league_toolkit::league_file::*;
-/// #
-/// let data = b"r3d2skltblahblahblahblah";
-/// let kind = identify_league_file(data);
-/// assert_eq!(kind, LeagueFileKind::Skeleton);
-/// ```
-///
-///
-/// ## Identifying from a reader
-/// ```
-/// # use std::fs::File;
-/// # use std::io::{self, Cursor, Read};
-/// # use league_toolkit::league_file::*;
-/// #
-/// let mut reader = Cursor::new([0x33, 0x22, 0x11, 0x00, 0xDE, 0xAD, 0xBE, 0xEF]);
-/// let mut buffer = [0; MAX_MAGIC_SIZE];
-/// reader.read(&mut buffer)?;
-///
-/// let kind = identify_league_file(&buffer);
-/// assert_eq!(kind, LeagueFileKind::SimpleSkin);
-/// # Ok::<(), io::Error>(())
-/// ```
-pub fn identify_league_file(data: &[u8]) -> LeagueFileKind {
-    for magic_byte in LEAGUE_FILE_MAGIC_BYTES.iter() {
-        if magic_byte.matches(data) {
-            return magic_byte.kind;
-        }
-    }
-
-    LeagueFileKind::Unknown
-}
