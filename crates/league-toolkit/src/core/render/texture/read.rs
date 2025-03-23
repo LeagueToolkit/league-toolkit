9d56c4be3d0b8e57ba0919448f4ddecc324f5008
Alan Panayotov <alanpanayotov@gmail.com>
2025-03-23 00:21:02 +0000

    fix (tex/dds): don't reset reader after reading magic

--- crates/league-toolkit/src/core/render/texture/read.rs
+++ crates/league-toolkit/src/core/render/texture/read.rs
@@ -8,7 +8,6 @@ use super::{tex, ReadError, Texture};
 impl Texture {
     pub fn from_reader<R: io::Read + io::Seek + ?Sized>(reader: &mut R) -> Result<Self, ReadError> {
         let magic = reader.read_u32::<LE>()?;
-        reader.seek(io::SeekFrom::Start(0))?;
 
         match TextureFileFormat::from_magic(magic) {
             TextureFileFormat::Unknown => Err(ReadError::UnknownTextureFormat(magic)),
