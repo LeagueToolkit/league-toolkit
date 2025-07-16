ecc106bfa0cf9da0128b6a106081676d75adb80e
Alan Panayotov <alanpanayotov@gmail.com>
2025-07-16 15:25:47 +0100

    wip: restructure + working ltk_wad

--- crates/league-toolkit/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod core;
-pub mod league_file;
-pub mod util;
---
+++ crates/ltk_wad/src/file_ext.rs
@@ -0,0 +1,25 @@
+use crate::WadChunkCompression;
+use ltk_file::LeagueFileKind;
+
+pub trait FileExt {
+    /// Get the ideal compression for this file type.
+    ///
+    /// # Examples
+    /// ```
+    /// # use league_toolkit::league_file::*;
+    /// # use league_toolkit::core::wad::WadChunkCompression;
+    /// #
+    /// assert_eq!(LeagueFileKind::Animation.ideal_compression(), WadChunkCompression::Zstd);
+    /// assert_eq!(LeagueFileKind::WwisePackage.ideal_compression(), WadChunkCompression::None);
+    /// ```
+    fn ideal_compression(&self) -> WadChunkCompression;
+}
+
+impl FileExt for LeagueFileKind {
+    fn ideal_compression(&self) -> WadChunkCompression {
+        match self {
+            LeagueFileKind::WwisePackage | LeagueFileKind::WwiseBank => WadChunkCompression::None,
+            _ => WadChunkCompression::Zstd,
+        }
+    }
+}
