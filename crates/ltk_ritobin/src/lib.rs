4087c55440d5e6675f5f0434945efa59cdedddac
Alan Panayotov <alanpanayotov@gmail.com>
2025-10-07 19:57:01 +0100

    wip

--- crates/ltk_ritobin/src/lib.rs
+++ crates/ltk_ritobin/src/lib.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Seek};
 
+use literals::Block;
 use nom::{
     branch::alt,
     bytes::complete::{escaped, is_not, tag, take_till, take_until},
@@ -41,26 +42,78 @@ pub fn bin_type(input: &str) -> IResult<&str, (&str, Option<Vec<&str>>)> {
     .parse(input)
 }
 
+#[derive(Debug, Clone)]
+pub enum Value<'a> {
+    Block(Block<'a>),
+    Keyword(&'a str),
+    String(&'a str),
+    Bool(bool),
+    Other(&'a str),
+}
+
+pub fn bin_value(input: &str) -> IResult<&str, Value<'_>> {
+    alt((
+        literals::boolean.map(Value::Bool),
+        literals::string.map(Value::String),
+        literals::hexadecimal.map(Value::Other),
+        literals::binary.map(Value::Other),
+        literals::octal.map(Value::Other),
+        literals::float.map(Value::Other),
+        literals::integer.map(Value::Other),
+        literals::block.map(Value::Block),
+    ))
+    .parse(input)
+}
+
+#[derive(Debug, Clone)]
+pub struct Type<'a> {
+    pub value: &'a str,
+    pub subtypes: Option<Vec<&'a str>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Statement<'a> {
+    pub name: Value<'a>,
+    pub kind: Option<Type<'a>>,
+    pub value: Value<'a>,
+}
+
+pub fn statement(input: &str) -> IResult<&str, Statement<'_>> {
+    let (input, (name, kind, value)) = (
+        alt((
+            ws(literals::string).map(Value::String),
+            ws(literals::hexadecimal).map(Value::Other),
+            ws(take_till(|c: char| {
+                c.is_whitespace() || c == ':' || c == '='
+            }))
+            .map(Value::Keyword),
+        )), // name
+        opt(preceded(ws(char(':')), ws(bin_type))),
+        preceded(ws(char('=')), ws(bin_value)),
+    )
+        .parse(input)?;
+
+    Ok((
+        input,
+        Statement {
+            name,
+            kind: kind.map(|(value, subtypes)| Type { value, subtypes }),
+            value,
+        },
+    ))
+}
+
+pub fn comment(input: &str) -> IResult<&str, &str> {
+    recognize((char('#'), is_not("\n\r"))).parse(input)
+}
+pub fn comments(input: &str) -> IResult<&str, &str> {
+    recognize(many0(terminated(comment, blank_space()))).parse(input)
+}
+pub fn blank(input: &str) -> IResult<&str, ()> {
+    value((), preceded(blank_space(), comments)).parse(input)
+}
+
 pub fn parse(text: &str) -> IResult<&str, &str> {
-    let comment = recognize((char('#'), is_not("\n\r")));
-    let comments = recognize(many0(terminated(comment, blank_space())));
-
-    let blank = value((), preceded(blank_space(), comments));
-    let bin_value = alt((
-        literals::decimal,
-        literals::float,
-        literals::string,
-        literals::binary,
-        literals::hexadecimal,
-        literals::octal,
-    ));
-    // let bin_value = literals::decimal;
-    let statement = (
-        ws(is_not("=:")), // name
-        opt(preceded(char(':'), ws(bin_type))),
-        preceded(char('='), ws(bin_value)),
-    );
-
     let mut statements = many0((blank, statement));
 
     let (input, stmt) = statements.parse(text)?;
@@ -76,10 +129,11 @@ mod tests {
     #[test]
     fn bin_types() {
         #[allow(clippy::type_complexity)]
-        let cases: [(&str, (&str, Option<Vec<&str>>)); 3] = [
+        let cases: [(&str, (&str, Option<Vec<&str>>)); 4] = [
             ("string", ("string", None)),
             ("u32", ("u32", None)),
             (" list[string]  ", ("list", Some(vec!["string"]))),
+            ("map[hash,embed]", ("map", Some(vec!["hash", "embed"]))),
         ];
         for (test, out) in cases {
             let (_, got) = bin_type(test).unwrap();
