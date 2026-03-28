2db21c1aaf0a93057faf274be6323235232bf411
Alan Panayotov <alanpanayotov@gmail.com>
2026-03-28 23:42:32 +0000

    doc: lib.rs

--- crates/ltk_ritobin/src/lib.rs
+++ crates/ltk_ritobin/src/lib.rs
@@ -5,44 +5,55 @@
 //!
 //! # Example
 //!
-//! ```rust,ignore
-//! use ltk_ritobin::{parse, write};
+//! ```rust
+//! use ltk_ritobin::{Cst, Print as _};
 //!
 //! // Parse ritobin text
 //! let text = r#"
 //! #PROP_text
-//! type: string = "PROP"
+//! type: string = "my_str"
 //! version: u32 = 3
+//! linked: list[string] = [ ]
+//! entries: map[hash, embed] = { }
 //! "#;
 //!
-//! let file = parse(text).unwrap();
-//! let tree = file.to_bin_tree();
+//! let cst = Cst::parse(text);
+//! assert!(cst.errors.is_empty());
+//!
+//! let (bin, bin_errors) = cst.build_bin(text);
+//! assert!(bin_errors.is_empty());
 //!
 //! // Write back to text
-//! let output = write(&tree).unwrap();
+//! let output = bin.print().unwrap();
+//!
+//! assert_eq!(text, output);
 //! ```
 //!
 //! # Error Reporting
 //!
-//! Parse errors include span information compatible with [`miette`] for rich
-//! error reporting with source highlighting:
+//! For resilient parsing, errors can appear embedded as nodes into the concrete syntax tree (cst), or as a list in the [`Cst`] struct. This
+//! allows for more versatile behaviour for things like pretty-printing technically invalid trees,
+//! since parsing will always result in a cst.
 //!
-//! ```rust,ignore
-//! use ltk_ritobin::parse;
-//! use miette::Report;
+//! The same handling of errors is done in the type-checker (when building a [`ltk_meta::Bin`]), to
+//! always provide a best effort construction.
 //!
-//! let text = "test: badtype = 42";
-//! match parse(text) {
-//!     Ok(file) => { /* ... */ }
-//!     Err(e) => {
-//!         // Print with miette formatting
-//!         eprintln!("{:?}", Report::new(e));
-//!     }
-//! }
+//! ```rust
+//! use ltk_ritobin::{Cst, cst::FlatErrors};
+//!
+//! let text = "test: u32 = 4!!2";
+//! let cst = Cst::parse(text);
+//! assert_eq!(cst.errors.len(), 0); // no 'top level' errors in the CST
+//!
+//! // helper that walks the CST, and returns all error
+//! // nodes as a list
+//! let flat_errors = FlatErrors::walk(&cst);
+//! assert_eq!(flat_errors.len(), 1); // the unexpected "!!!" in the value
+//!
 //! ```
 
-// Nom-style parsers use elided lifetimes extensively
-#![allow(mismatched_lifetime_syntaxes)]
+#[allow(unused)] // for doc link above
+use ltk_meta::Bin;
 
 pub mod cst;
 pub mod hashes;
@@ -55,3 +66,4 @@ pub use hashes::*;
 pub use types::*;
 
 pub use cst::Cst;
+pub use print::Print;
