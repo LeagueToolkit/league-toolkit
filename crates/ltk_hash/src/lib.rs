1d9dfc2320e8e9464e7bc8d10a07458dace1d21a
Alan Panayotov <alanpanayotov@gmail.com>
2026-06-20 14:55:19 +0100

    feat(hash)!: loads of hash type impls + rename 'from_str' -> 'hash_str' + serde flag

--- crates/ltk_hash/src/lib.rs
+++ crates/ltk_hash/src/lib.rs
@@ -1,77 +1,174 @@
 //! Other utilities (hashing, etc)
 
-pub mod elf;
-mod fnv1a;
+mod impls;
+
+use std::{
+    fmt::{Display, LowerHex},
+    io::{Read, Write},
+    num::ParseIntError,
+    ops::{Deref, DerefMut},
+    str::FromStr,
+};
+
+pub use impls::elf;
+use impls::fnv1a;
+
 use xxhash_rust::xxh64::xxh64;
 
 pub trait Hash: std::hash::Hash + Eq + Ord + Copy {
-    fn from_str(src: impl AsRef<str>) -> Self;
+    fn hash_str(src: impl AsRef<str>) -> Self;
 }
 
 /// Wad path hashes - case insensitive
 #[repr(transparent)]
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, std::hash::Hash)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, std::hash::Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct WadHash(pub u64);
 
 impl Hash for WadHash {
-    fn from_str(src: impl AsRef<str>) -> Self {
+    fn hash_str(src: impl AsRef<str>) -> Self {
         Self(xxh64(src.as_ref().to_ascii_lowercase().as_bytes(), 0))
+    }
+}
+
+impl Display for WadHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        LowerHex::fmt(self, f)
+    }
+}
+impl LowerHex for WadHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        LowerHex::fmt(&self.0, f)
+    }
+}
+impl FromStr for WadHash {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_str_radix(s, 16)
+    }
+}
+impl WadHash {
+    pub const fn from_str_radix(src: &str, radix: u32) -> Result<Self, ParseIntError> {
+        match u64::from_str_radix(src, radix) {
+            Ok(v) => Ok(Self(v)),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl From<u64> for WadHash {
+    fn from(value: u64) -> Self {
+        Self(value)
+    }
+}
+impl AsRef<u64> for WadHash {
+    fn as_ref(&self) -> &u64 {
+        &self.0
+    }
+}
+impl AsMut<u64> for WadHash {
+    fn as_mut(&mut self) -> &mut u64 {
+        &mut self.0
+    }
+}
+impl Deref for WadHash {
+    type Target = u64;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+impl DerefMut for WadHash {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
     }
 }
 
 /// Used for bin field/class/property names, etc. - case insensitive
 #[repr(transparent)]
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, std::hash::Hash)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, std::hash::Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct BinHash(pub u32);
 
 impl Hash for BinHash {
-    fn from_str(src: impl AsRef<str>) -> Self {
+    fn hash_str(src: impl AsRef<str>) -> Self {
         Self(fnv1a::hash_lower(src.as_ref()))
     }
 }
 
-pub struct FatHash<H: Hash, V: AsRef<str>> {
-    pub hash: H,
-    pub value: Option<V>,
+impl Display for BinHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        LowerHex::fmt(self, f)
+    }
 }
+impl LowerHex for BinHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        LowerHex::fmt(&self.0, f)
+    }
+}
+impl FromStr for BinHash {
+    type Err = ParseIntError;
 
-impl<H: Hash, V: AsRef<str>> From<H> for FatHash<H, V> {
-    fn from(value: H) -> Self {
-        Self {
-            hash: value,
-            value: None,
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_str_radix(s, 16)
+    }
+}
+impl BinHash {
+    pub const fn from_str_radix(src: &str, radix: u32) -> Result<Self, ParseIntError> {
+        match u32::from_str_radix(src, radix) {
+            Ok(v) => Ok(Self(v)),
+            Err(e) => Err(e),
         }
     }
 }
 
-impl<H: Hash, V: AsRef<str>> FatHash<H, V> {
-    pub fn new(source: V) -> Self {
-        Self {
-            hash: H::from_str(&source),
-            value: Some(source),
-        }
+impl From<u32> for BinHash {
+    fn from(value: u32) -> Self {
+        Self(value)
+    }
+}
+impl AsRef<u32> for BinHash {
+    fn as_ref(&self) -> &u32 {
+        &self.0
+    }
+}
+impl AsMut<u32> for BinHash {
+    fn as_mut(&mut self) -> &mut u32 {
+        &mut self.0
+    }
+}
+impl Deref for BinHash {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+impl DerefMut for BinHash {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
     }
 }
 
-impl<H: Hash, V: AsRef<str>> std::hash::Hash for FatHash<H, V> {
-    fn hash<H2: std::hash::Hasher>(&self, state: &mut H2) {
-        self.hash.hash(state)
+use byteorder::{ByteOrder, ReadBytesExt as _, WriteBytesExt as _};
+pub trait ReadBytesExt: Read {
+    fn read_bin_hash<O: ByteOrder>(&mut self) -> std::io::Result<BinHash> {
+        Ok(self.read_u32::<O>()?.into())
     }
-}
-impl<H: Hash, V: AsRef<str>> PartialOrd for FatHash<H, V> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl<H: Hash, V: AsRef<str>> Ord for FatHash<H, V> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.hash.cmp(&other.hash)
+    fn read_wad_hash<O: ByteOrder>(&mut self) -> std::io::Result<WadHash> {
+        Ok(self.read_u64::<O>()?.into())
     }
 }
 
-impl<H: Hash, V: AsRef<str>> PartialEq for FatHash<H, V> {
-    fn eq(&self, other: &Self) -> bool {
-        self.hash == other.hash
+impl<R: Read + ?Sized> ReadBytesExt for R {}
+pub trait WriteBytesExt: Write {
+    fn write_bin_hash<O: ByteOrder>(&mut self, hash: BinHash) -> std::io::Result<()> {
+        self.write_u32::<O>(*hash)
+    }
+    fn write_wad_hash<O: ByteOrder>(&mut self, hash: WadHash) -> std::io::Result<()> {
+        self.write_u64::<O>(*hash)
     }
 }
-impl<H: Hash, V: AsRef<str>> Eq for FatHash<H, V> {}
+
+impl<W: Write + ?Sized> WriteBytesExt for W {}
